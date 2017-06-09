17f2845c9cc5ec8835bbe2b615c9bfc2d00cd865
naveenram8 <naveenram8@gmail.com>
2017-06-09 14:31:20 +0530

    Create happy.java

--- happy.java
+++ happy.java
@@ -1,39 +1,31 @@
 import java.io.*;
 import java.util.*;
-public class happyno
+class happyno
 {
-    public static void main(String args[])
-    {
-        int n,a,b,c,d,e,f,g,h,i,j,k,l,m,o,temp,x,y,z,p,q;
-        Scanner s=new Scanner(System.in);
-        n=s.nextInt();
-            temp=n;
-            a=temp%10;
-            b=a*a;
-            c=temp/10;
-            d=c*c;
-            e=b+d;
-            x=e%10;
-            y=x*x;
-            z=e/10;
-            p=z*z;
-            q=y+p;
-        f=q%10;
-        g=f*f;
-        h=q/10;
-        j=h%10;
-        k=j*j;
-        l=h/10;
-        m=l*l;
-        o=1;
-        if(o==1)
-        {
-        System.out.println("true");
-        }
-        else
-        {
-            System.out.println("not happy");
-        }
-    }
+static int sum(int n)
+{
+int s,i,d;
+s=0;
+for(i=n;i!=0;i=i/10)
+{
+d=i%10;
+s=s+d*d;
 }
-
+return s;
+}
+public static void main(String args[])
+{
+Scanner sn=new Scanner(System.in);
+int num,s;
+num=sn.nextInt();
+s=num;
+while(s>9)
+{
+s=sum(s);
+}
+if(s==1)
+System.out.println(num+" is happy number");
+else
+System.out.println(num+" is not happy number");
+}
+}
