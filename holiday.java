aa6c8ee58a2ca095fbb460caaf6e72ceb556681f
naveenram8 <naveenram8@gmail.com>
2017-06-09 14:02:30 +0530

    Create holiday.java

---
+++ holiday.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class holiday{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        String a[]=new String[]{"sunday","monday","tuesday","wednesday","thursday","friday","saturday"};
+        String working_day=s.next();
+        working_day
+        if(working_day.equals(a[0])){
+            System.out.println("TRUE");
+        }else{
+            System.out.println("FALSE");
+        }
+    }
+}
