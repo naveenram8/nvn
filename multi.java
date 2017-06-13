1d7df3df7b6b68c10e929e30e656c7fbb1453eed
naveenram8 <naveenram8@gmail.com>
2017-06-13 09:29:31 +0530

    Create multi.java

---
+++ multi.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class multi{
+    public static void main(String ar[]){
+        Scanner s=new Scanner(System.in);
+        String s1=s.next();
+        String s2=s.next();
+        int a=Integer.parseInt(s1);
+        int b=Integer.parseInt(s2);
+        int c=a*b;
+        String s3=c+"";
+        System.out.println(s3);
+    }
+}
