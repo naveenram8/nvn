0e65a368be007b48c335a8e309ecc9f06222881e
naveenram8 <naveenram8@gmail.com>
2017-06-13 09:11:55 +0530

    Create stringtoint.java

--- stringtoint.java
+++ stringtoint.java
@@ -2,5 +2,8 @@ import java.io.*;
 import java.util.*;
 public class stringtoint{
 public static void main(String args[]){
+  Scanner s=new Scanner(System.in);
+  String s1=s.next();
+  System.out.println(Integer.parseInt(s1));
 }
 }
